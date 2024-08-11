b570988d79e456952e8ee375739e2883a4b248f7
Jenny Martinez <lajennylove@users.noreply.github.com>
2024-08-11 15:01:30 -0400

    fix: footer reorder

--- resources/views/sections/footer.blade.php
+++ resources/views/sections/footer.blade.php
@@ -3,24 +3,22 @@
         <div class="w-full h-auto py-3 px-2 flex flex-col items-start justify-center md:w-1/4 md:py-0 md:px-5">
             <div class="w-full h-auto py-3 text-2xl font-DMSerifDisplay font-bold md:text-2xl">{!! get_field('footer_col_1_title', 'option') !!}</div>
             <p class="text-stone-500">{!! get_field('footer_col_1_content', 'option') !!}</p>
-        </div>
-        <div class="w-full h-auto py-3 px-2 flex flex-col items-start justify-center md:w-1/4 md:py-0 md:px-5">
-            <div class="w-full h-auto py-3 text-2xl font-DMSerifDisplay font-bold md:text-2xl">{!! get_field('footer_col_2_title', 'option') !!}</div>
-            <p class="text-stone-500">{!! get_field('footer_col_2_content', 'option') !!}</p>
             <div class="w-full h-auto mt-3 py-3 flex flex-row items-center justify-center md:mt-5">
-                <a class="w-1/4 h-auto flex items-center justify-center" href="{!! get_field('facebook', 'option') !!}" target="_blank">
-                    <x-socialicon icon="facebook" size="w-8 h-8" />
-                </a>
-                <a class="w-1/4 h-auto flex items-center justify-center" href="{!! get_field('twitter', 'option') !!}" target="_blank">
-                    <x-socialicon icon="x" size="w-8 h-8" />
-                </a>
                 <a class="w-1/4 h-auto flex items-center justify-center" href="{!! get_field('linkedin', 'option') !!}" target="_blank">
                     <x-socialicon icon="linkedin" size="w-8 h-8" />
+                </a>
+                <a class="w-1/4 h-auto flex items-center justify-center" href="{!! get_field('facebook', 'option') !!}" target="_blank">
+                    <x-socialicon icon="facebook" size="w-8 h-8" />
                 </a>
                 <a class="w-1/4 h-auto flex items-center justify-center" href="https://www.flickr.com/people/{!! get_field('flickr_username', 'option') !!}/" target="_blank">
                     <x-socialicon icon="flickr" size="w-8 h-8" />
                 </a>
             </div>
+        </div>
+        <div class="w-full h-auto py-3 px-2 flex flex-col items-start justify-center md:w-1/4 md:py-0 md:px-5">
+            <div class="w-full h-auto py-3 text-2xl font-DMSerifDisplay font-bold md:text-2xl">{!! get_field('footer_col_2_title', 'option') !!}</div>
+            <p class="text-stone-500">{!! get_field('footer_col_2_content', 'option') !!}</p>
+
         </div>
         <div class="w-full h-auto py-3 px-2 flex flex-col items-start justify-center md:w-1/4 md:py-0 md:px-5">
             <div class="w-full h-auto py-3 text-2xl font-DMSerifDisplay font-bold md:text-2xl">{!! get_field('footer_col_3_title', 'option') !!}</div>
@@ -37,4 +35,4 @@
         Copyright {{ date('Y') }} | {!! bloginfo( 'title' ); !!}
     </div>
 </div>
-
+<div class="w-9/12"></div>
